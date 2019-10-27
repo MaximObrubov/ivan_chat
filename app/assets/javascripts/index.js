7c030730aff52b5caec3335f5bde6513100fbe85
Maxim Obrubov <m.obrubov@yandex.ru>
2019-10-27 20:42:37 +0400

    show answer messages + some styles

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,30 +1,50 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-
-
 ;(function (w, $) {
   
   function Chat() {
     this.$root = $('.chat');
-    this.$footer = this.$root.find('.chat-header');
+    this.$header = this.$root.find('.chat-header');
     this.$body = this.$root.find('.chat-body');
     this.$footer = this.$root.find('.chat-footer');
+    this.$input = this.$footer.find('.chat-footer-input');
+    this.$sendBtn = this.$footer.find('.chat-footer-send-button');
   }
     
   Chat.prototype.subscribe = function () {
-    var self = this,
-        $input = this.$footer.find('.chat-footer-input'),
-        $sendBtn = this.$footer.find('.chat-footer-send-button');
+    var self = this;
     
-    $input.on('keyup', function (e) {
+    // visual block to send empty message
+    self.$input.on('keyup', function (e) {
       var value = this.value;
-    
-      $sendBtn.prop('disabled', function (index, _btnValue) {
+      self.$sendBtn.prop('disabled', function (index, _btnValue) {
         return !(value && value.length && value.length > 0);
       });      
     });
-    this._send("chat subscribed");
+    
+    // subscribe on send message
+    document.body.addEventListener('ajax:success', self._onMessageReceived.bind(self));
   };
+  
+  
+  Chat.prototype._onMessageReceived = function (event) {
+    var self = this,
+        detail = event.detail,
+        data = detail[0], 
+        status = detail[1], 
+        xhr = detail[2];    
+    this.$body.append(self._drawMessage(data.message));
+    self.$input.val('');
+    self.$sendBtn.prop('disabled', 'disabled');
+  };
+  
+  
+  Chat.prototype._drawMessage = function (message) {
+    var $message = $("<div/>", {
+          "class": "chat-body-message",
+          "text": message,
+        });
+    return $message;
+  };
+  
   
   w.Chat = Chat;
 
