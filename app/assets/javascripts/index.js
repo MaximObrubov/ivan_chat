548d7197f6212aa8ae5ed729d46b2e1b56cac1b3
Maxim Obrubov <m.obrubov@yandex.ru>
2019-10-27 15:58:46 +0400

    added input and button subscriptions

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -5,15 +5,37 @@
 ;(function (w, $) {
   
   function Chat() {
-    this.$root = $("body")
+    this.$root = $('.chat');
+    this.$footer = this.$root.find('.chat-header');
+    this.$body = this.$root.find('.chat-body');
+    this.$footer = this.$root.find('.chat-footer');
   }
     
   Chat.prototype.subscribe = function () {
-    this.send("chat subscribed");
+    var self = this,
+        $input = this.$footer.find('.chat-footer-input'),
+        $sendBtn = this.$footer.find('.chat-footer-send-button');
+    
+    $input.on('keyup', function (e) {
+      var value = this.value;
+      
+      $sendBtn.prop('disabled', function (index, _btnValue) {
+        return !(value && value.length && value.length > 0);
+      });      
+    });
+    
+    
+    $sendBtn.on("click", function (e) {
+      self._send($input[0].value);
+    })
+    
+    this._send("chat subscribed");
   };
   
   
-  Chat.prototype.send = function (message) {  
+  // PRIVATE METHODS
+  
+  Chat.prototype._send = function (message) {  
     console.group("%c Custom log:", "background: lightgreen; color: orange; font-size: 16px;");
     console.log(message);
     console.groupEnd();
