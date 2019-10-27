6108a6f60967f3bab5480653aa9eff19ecd8cd22
Maxim Obrubov <m.obrubov@yandex.ru>
2019-10-27 14:46:51 +0400

    index controller and basic empty views

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,7 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+
+
+console.group("%c Custom log:", "background: lightgreen; color: orange; font-size: 16px;");
+console.log("yahoo");
+console.groupEnd();
